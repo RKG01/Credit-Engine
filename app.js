3264c0cb8c0aa86eb127568543c5b4bb1c433bea
rahul <rk5849193@gmail.com>
2025-06-14 12:09:02 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./src/config/db');
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 connectDB();
